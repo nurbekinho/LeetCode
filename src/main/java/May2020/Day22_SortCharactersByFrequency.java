0755f9d73ba3e287ccf3a07d31edb5eeb5a67a5e
Nurbek T <nurbekinho@gmail.com>
2020-05-23 16:01:21 +0200

    Solve Day 22 challenge

---
+++ src/main/java/May2020/Day22_SortCharactersByFrequency.java
@@ -0,0 +1,30 @@
+package May2020;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Day22_SortCharactersByFrequency {
+    public static void main(String[] args) {
+        System.out.println(frequencySort("tree"));
+    }
+
+    public static String frequencySort(String s) {
+        StringBuilder result = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) map.put(c, map.containsKey(c) ? map.get(c) + 1 : 1);
+
+        Map<Character, Integer> sorted = new LinkedHashMap<>();
+        map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> sorted.put(x.getKey(), x.getValue()));
+
+        sorted.forEach((key, value) -> {
+            for (int i = 0; i < value; i++) result.append(key);
+        });
+
+        return result.toString();
+    }
+}
