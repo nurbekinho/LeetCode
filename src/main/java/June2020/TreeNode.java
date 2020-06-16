fd1eab84ab3d9de764e90e8383106e5731acd6e6
Nurbek T <nurbekinho@gmail.com>
2020-06-16 17:25:53 +0200

    Solve Day 15 challenge

--- src/main/java/June2020/TreeNode.java
+++ src/main/java/June2020/TreeNode.java
@@ -1,5 +1,8 @@
 package June2020;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     int val;
     TreeNode left;
@@ -17,5 +20,29 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static void printNodesDFS(TreeNode root) {
+        if (root != null) {
+            System.out.println(root.val);
+
+            if (root.left != null) printNodesDFS(root.left);
+            if (root.right != null) printNodesDFS(root.right);
+        }
+    }
+
+    public static void printNodesBFS(TreeNode root) {
+        if (root != null) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                TreeNode current = queue.poll();
+                System.out.println(current.val);
+
+                if (current.left != null) queue.add(current.left);
+                if (current.right != null) queue.add(current.right);
+            }
+        }
     }
 }
