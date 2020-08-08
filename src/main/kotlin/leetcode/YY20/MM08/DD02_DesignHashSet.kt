eb2cc0bf584cc20432d0ff02ccff1bface3f488a
Nurbek T <nurbekinho@gmail.com>
2020-08-08 23:29:51 +0200

    Solve Day 2 challenge

---
+++ src/main/kotlin/leetcode/YY20/MM08/DD02_DesignHashSet.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var obj = MyHashSet()
+    obj.add(1)
+    println(obj)
+    obj.remove(2)
+    println(obj)
+    var param3 = obj.contains(3)
+    println(param3)
+}
+
+class MyHashSet() {
+    var data: MutableSet<Int> = mutableSetOf()
+
+    fun add(key: Int) {
+        data.add(key)
+    }
+
+    fun remove(key: Int) {
+        data.remove(key)
+    }
+
+    /** Returns true if this set contains the specified element */
+    fun contains(key: Int) = data.contains(key)
+}
