8c44050fce41b746da7a4d6d1056b6cba94fa95e
Nurbek T <nurbekinho@gmail.com>
2020-09-24 11:06:58 +0200

    Solve Day 25 challenge

---
+++ src/main/kotlin/leetcode/YY20/MM09/DD25_LargestNumber.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println(largestNumber(intArrayOf(3, 30, 34, 5, 9)))
+}
+
+fun largestNumber(nums: IntArray): String {
+    val result = nums
+        .map { it.toString() }
+        .sortedWith { o1, o2 -> (o1.plus(o2).compareTo(o2.plus(o1))) }
+        //.sortedWith(Comparator { o1, o2 -> (o1.plus(o2).compareTo(o2.plus(o1))) })
+        .reversed()
+        .joinToString("")
+
+    return if (result[0] == '0') "0" else result
+}
