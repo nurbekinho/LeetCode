0bbfbdebc90d1f3a1a4c3a89f3543d90b1138aa6
Nurbek T <nurbekinho@gmail.com>
2020-09-30 18:17:50 +0200

    Solve Day 29 challenge

---
+++ src/main/kotlin/leetcode/YY20/MM09/DD29_WordBreak.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println(wordBreak("leetcode", listOf("leet", "code")))
+    println(wordBreak("applepenapple", listOf("apple", "pen")))
+}
+
+fun wordBreak(s: String, wordDict: List<String>): Boolean {
+    val map = mutableMapOf<String, Boolean>()
+    return findRecursively(s, wordDict, map)
+}
+
+fun findRecursively(s: String, wordDict: List<String>, map: MutableMap<String, Boolean>): Boolean {
+    if (s.isEmpty()) return true
+    if (map[s] != null) return map[s]!!
+
+    for (word in wordDict) {
+        if (s.startsWith(word) && findRecursively(s.substring(word.length), wordDict, map)) {
+            map[s.substring(word.length)] = true
+            return true
+        }
+    }
+
+    map[s] = false
+
+    return false
+}
