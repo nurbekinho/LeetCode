45b8a4615c22785ffbed4038fb45ac640962ef81
Nurbek T <nurbekinho@gmail.com>
2020-11-07 21:12:37 +0100

    Solve LC 310 (Clean up)

--- src/main/kotlin/leetcode/Problems/LC0310_MinimumHeightTrees.kt
+++ src/main/kotlin/leetcode/Problems/LC0310_MinimumHeightTrees.kt
@@ -27,38 +27,3 @@ fun findMinHeightTrees(n: Int, edges: Array<IntArray>): List<Int> {
 
     return emptyList()
 }
-
-/*
-if(n == 1) return Collections.singletonList(0);
-
-List<Set<Integer>> graph = new ArrayList();
-IntStream.range(0, n)
-         .forEach(i -> graph.add(new HashSet()));
-
-Arrays.stream(edges)
-     .forEach(edge -> {
-         graph.get(edge[0]).add(edge[1]);
-         graph.get(edge[1]).add(edge[0]);
-     });
-
-List<Integer> leafNodes = IntStream.range(0, n)
-                                   .filter(i -> graph.get(i).size() == 1)
-                                   .boxed()
-                                   .collect(Collectors.toList());
-
-List<Integer> answer = new ArrayList();
-while(n > 2) {
-    n -= leafNodes.size();
-    List<Integer> newLeaves = leafNodes.stream()
-                                       .map(leaf -> {
-                                           int neigh = graph.get(leaf).iterator().next();
-                                           graph.get(neigh).remove(leaf);
-                                           return neigh;
-                                       })
-                                       .filter(neigh -> graph.get(neigh).size() == 1)
-                                       .collect(Collectors.toList());
-    leafNodes = newLeaves;
-}
-
-return leafNodes;
- */
