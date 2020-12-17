1ede2d00a079d28a6e26a76e5f995ee4469029a0
Nurbek T <nurbekinho@gmail.com>
2020-12-17 19:15:11 +0100

    Solve LC 657

---
+++ src/main/kotlin/leetcode/Problems/LC0657_RobotReturnToOrigin.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println(judgeCircle("UD"))
+}
+
+fun judgeCircle(moves: String): Boolean {
+    var x = 0
+    var y = 0
+    for (move in moves) {
+        when (move) {
+            'U' -> y++
+            'D' -> y--
+            'R' -> x++
+            'L' -> x--
+        }
+    }
+
+    return x == 0 && y == 0
+}
